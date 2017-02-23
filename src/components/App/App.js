88bb8ee7807588125033058866d380696403c098
Kevin Cooper <cooperka92@gmail.com>
2017-02-22 23:12:08 -0500

    :package: Add social links; redo CSS in ems
    Based on font-size: 62.5%.
    Now 1em = 10px, and it respects browser font-size preferences.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,13 +9,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="main-header">
+          <img className="main-logo" src={logo} alt="logo" />
+          <div className="main-title">Kevin Cooper</div>
+          <div className="main-subtitle">Software Engineer</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="social-links">
+          <li className="social-link"><a href="https://www.facebook.com/cooperka">Facebook</a></li>
+          <li className="social-link"><a href="https://www.linkedin.com/in/cooperka92">LinkedIn</a></li>
+          <li className="social-link"><a href="https://github.com/cooperka">GitHub</a></li>
+        </ul>
       </div>
     );
   }
