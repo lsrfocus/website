48ebf42bc7ab40515a9e1f218d97021fceeec65f
Kevin Cooper <cooperka92@gmail.com>
2017-02-25 16:51:34 -0500

    :package: Use MUCH simpler/better hover animations

--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import './SocialLink.css';
 
@@ -12,42 +11,22 @@ class SocialLink extends Component {
     iconHover: PropTypes.string.isRequired,
   };
 
-  state = {
-    isHovering: false,
-  };
-
-  handleMouseOver() {
-    this.setState({ isHovering: true });
-  }
-
-  handleMouseOut() {
-    this.setState({ isHovering: false });
-  }
-
   render() {
-    const { isHovering } = this.state;
     const { name, url, icon, iconHover } = this.props;
 
     return (
       <li className="social-link">
-        <a
-          className="social-link"
-          href={url}
-          onMouseOver={() => this.handleMouseOver()}
-          onMouseOut={() => this.handleMouseOut()}
-        >
-          <ReactCSSTransitionGroup
-            transitionName="hover"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}
-          >
-            <img
-              key={isHovering}
-              className="social-icon"
-              src={isHovering ? iconHover : icon}
-              alt={name}
-            />
-          </ReactCSSTransitionGroup>
+        <a className="social-link" href={url}>
+          <img
+            className={['social-icon', 'black'].join(' ')}
+            src={icon}
+            alt={name}
+          />
+          <img
+            className={['social-icon', 'color'].join(' ')}
+            src={iconHover}
+            alt={name}
+          />
         </a>
       </li>
     );
