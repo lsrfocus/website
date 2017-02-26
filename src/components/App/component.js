0fa4b41919c270eb571d26c695e98ac5cdbaef6b
Kevin Cooper <cooperka92@gmail.com>
2017-02-26 10:36:25 -0500

    :package: Extract social links into their own file

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,45 +1,11 @@
 import React, { Component } from 'react';
 
+import profilePhoto from '../../images/profile-2017-web@800.jpg';
+import SOCIAL_LINKS from '../../socialLinks';
+
 import SocialLink from '../SocialLink';
 
-import profilePhoto from '../../images/profile-2017-web@800.jpg';
-
 import './styles.css';
-
-/* eslint-disable global-require */
-const SOCIAL_LINKS = [
-  {
-    name: 'Facebook',
-    url: 'https://www.facebook.com/cooperka',
-    icon: require('../../images/Facebook-black.svg'),
-    iconHover: require('../../images/Facebook-color.svg'),
-  },
-  {
-    name: 'LinkedIn',
-    url: 'https://www.linkedin.com/in/cooperka92',
-    icon: require('../../images/LinkedIn-black.svg'),
-    iconHover: require('../../images/LinkedIn-color.svg'),
-  },
-  {
-    name: 'GitHub',
-    url: 'https://github.com/cooperka',
-    icon: require('../../images/Github-black.svg'),
-    iconHover: require('../../images/Github-color.svg'),
-  },
-  {
-    name: 'Medium',
-    url: 'https://medium.com/@cooperka',
-    icon: require('../../images/Medium-black.svg'),
-    iconHover: require('../../images/Medium-color.svg'),
-  },
-  {
-    name: 'Tumblr',
-    url: 'https://mentalmorsels.tumblr.com/',
-    icon: require('../../images/Tumblr-black.svg'),
-    iconHover: require('../../images/Tumblr-color.svg'),
-  },
-];
-/* eslint-enable */
 
 class App extends Component {
 
---
+++ src/socialLinks.js
@@ -0,0 +1,34 @@
+/* eslint-disable global-require */
+export default [
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/cooperka',
+    icon: require('./images/Facebook-black.svg'),
+    iconHover: require('./images/Facebook-color.svg'),
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/cooperka92',
+    icon: require('./images/LinkedIn-black.svg'),
+    iconHover: require('./images/LinkedIn-color.svg'),
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/cooperka',
+    icon: require('./images/Github-black.svg'),
+    iconHover: require('./images/Github-color.svg'),
+  },
+  {
+    name: 'Medium',
+    url: 'https://medium.com/@cooperka',
+    icon: require('./images/Medium-black.svg'),
+    iconHover: require('./images/Medium-color.svg'),
+  },
+  {
+    name: 'Tumblr',
+    url: 'https://mentalmorsels.tumblr.com/',
+    icon: require('./images/Tumblr-black.svg'),
+    iconHover: require('./images/Tumblr-color.svg'),
+  },
+];
+/* eslint-enable */
