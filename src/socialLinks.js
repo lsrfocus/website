f47a0c3d9dec8c42a8ea1928944e448abb8f3145
Kevin Cooper <cooperka92@gmail.com>
2018-01-30 16:44:37 -0500

    :art: Rearrange social icons

--- src/socialLinks.js
+++ src/socialLinks.js
@@ -3,10 +3,10 @@
 /* eslint-disable global-require */
 export default [
   {
-    name: 'GitHub',
-    url: 'https://github.com/lsrfocus',
-    icon: require('./images/Github-black.svg'),
-    iconHover: require('./images/Github-color.svg'),
+    name: 'Facebook',
+    url: 'https://www.facebook.com/LsrFocus',
+    icon: require('./images/Facebook-black.svg'),
+    iconHover: require('./images/Facebook-color.svg'),
   },
   {
     name: 'LinkedIn',
@@ -15,16 +15,16 @@ export default [
     iconHover: require('./images/LinkedIn-color.svg'),
   },
   {
-    name: 'Facebook',
-    url: 'https://www.facebook.com/LsrFocus',
-    icon: require('./images/Facebook-black.svg'),
-    iconHover: require('./images/Facebook-color.svg'),
-  },
-  {
     name: 'Twitter',
     url: 'https://twitter.com/LsrFocus',
     icon: require('./images/Twitter-black.svg'),
     iconHover: require('./images/Twitter-color.svg'),
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/lsrfocus',
+    icon: require('./images/Github-black.svg'),
+    iconHover: require('./images/Github-color.svg'),
   },
 ];
 /* eslint-enable */
