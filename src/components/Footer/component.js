0b6441458e009a6d7e0b9971e8fd69bd9a191d96
Kevin Cooper <cooperka92@gmail.com>
2018-01-31 16:39:18 -0500

    :pencil: Update licensor and footer
    Now that LLC is filed

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <div className="Footer">
       <div>Full website coming soon.</div>
-      <div>&copy; 2018 Kevin Cooper</div>
+      <div>&copy; 2018 LSR software LLC</div>
     </div>
   );
 }
