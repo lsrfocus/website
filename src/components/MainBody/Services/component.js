8af5ba01f0674cdc1cf3eab51be9e73b4c8d1d4c
Kevin Cooper <cooperka92@gmail.com>
2018-02-05 17:51:21 -0500

    :art: Add basic grid and styles for Services

--- src/components/MainBody/Services/component.js
+++ src/components/MainBody/Services/component.js
@@ -8,6 +8,13 @@ function Services() {
   return (
     <div className="Services">
       <div className="title">Services</div>
+      <div className="subtitle">End-to-end software development</div>
+      <div className="grid-container">
+        <div className="grid-item">Web apps</div>
+        <div className="grid-item">Mobile apps</div>
+        <div className="grid-item">User testing</div>
+        <div className="grid-item">Strategic planning</div>
+      </div>
     </div>
   );
 }
