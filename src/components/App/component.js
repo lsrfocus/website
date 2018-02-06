c2985c6037fdfa5d5351e29a1b5d4c5a96a782f7
Kevin Cooper <cooperka92@gmail.com>
2018-02-06 15:35:59 -0500

    :sparkles: Install material-ui (with Reboot to normalize)

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Reboot from 'material-ui/Reboot';
 
 import routes from '../../routes';
 
@@ -14,6 +15,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Reboot />
         <NavBar />
         {routes.map((route) => (
           <Route key={route.path} exact path={route.path} component={route.component} />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { render } from 'react-snapshot';
+import 'typeface-roboto/index.css';
 
 import App from './components/App';
 
