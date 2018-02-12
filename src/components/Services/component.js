8d44c45380208ca4fbfc823abb54fd6162003cd7
Kevin Cooper <cooperka92@gmail.com>
2018-02-12 14:38:58 -0500

    :art: Make Services padding similar to About

--- src/components/Services/component.js
+++ src/components/Services/component.js
@@ -8,17 +8,23 @@ function Services() {
   return (
     <div className="Services">
       <div className="title">Services</div>
-      <div className="subtitle">Software development</div>
-      <div className="grid-container">
-        <div className="grid-item">Web apps</div>
-        <div className="grid-item">Mobile apps</div>
-        <div className="grid-item">User testing</div>
-        <div className="grid-item">Project planning</div>
+
+      <div className="section">
+        <div className="subtitle">Software development</div>
+        <div className="grid-container">
+          <div className="grid-item">Web apps</div>
+          <div className="grid-item">Mobile apps</div>
+          <div className="grid-item">User testing</div>
+          <div className="grid-item">Project planning</div>
+        </div>
       </div>
-      <div className="subtitle">Small business development</div>
-      <div className="grid-container">
-        <div className="grid-item">Strategic planning</div>
-        <div className="grid-item">Team building</div>
+
+      <div className="section">
+        <div className="subtitle">Small business development</div>
+        <div className="grid-container">
+          <div className="grid-item">Strategic planning</div>
+          <div className="grid-item">Team building</div>
+        </div>
       </div>
     </div>
   );
