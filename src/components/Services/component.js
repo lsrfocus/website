2af9a3298cc842f255441a01917da807187f9c9f
Kevin Cooper <cooperka92@gmail.com>
2018-02-12 11:01:47 -0500

    :sparkles: Add team building under 'business dev' service

--- src/components/Services/component.js
+++ src/components/Services/component.js
@@ -8,12 +8,17 @@ function Services() {
   return (
     <div className="Services">
       <div className="title">Services</div>
-      <div className="subtitle">End-to-end software development</div>
+      <div className="subtitle">Software development</div>
       <div className="grid-container">
         <div className="grid-item">Web apps</div>
         <div className="grid-item">Mobile apps</div>
         <div className="grid-item">User testing</div>
+        <div className="grid-item">Project planning</div>
+      </div>
+      <div className="subtitle">Small business development</div>
+      <div className="grid-container">
         <div className="grid-item">Strategic planning</div>
+        <div className="grid-item">Team building</div>
       </div>
     </div>
   );
