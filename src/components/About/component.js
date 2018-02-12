bd7c256bb370175d8013b17c90b6229c2cff8c53
Kevin Cooper <cooperka92@gmail.com>
2018-02-12 14:56:27 -0500

    :art: Refine the About wording a bit

--- src/components/About/component.js
+++ src/components/About/component.js
@@ -13,13 +13,13 @@ function About() {
         <div className="subtitle">What does LSR mean?</div>
 
         <div className="header">L - Lovable</div>
-        <div className="description">People should love using your stuff.</div>
+        <div className="description">People should love your stuff.</div>
 
         <div className="header">S - Simple</div>
         <div className="description">Your stuff should be understandable and straightforward.</div>
 
         <div className="header">R - Refined</div>
-        <div className="description">Your stuff should improve over time; it{"'"}s probably not perfect yet.</div>
+        <div className="description">Your stuff should improve over time; it{"'"}s probably not perfect yet (and that{"'"}s okay).</div>
       </div>
 
       <div className="section">
@@ -33,7 +33,7 @@ function About() {
           <a href="https://steveblank.com/2015/05/06/build-measure-learn-throw-things-against-the-wall-and-see-if-they-work/">
             Build Measure Learn
           </a>
-          . I highly recommend reading through those links to know more.
+          . I highly recommend reading through those links to learn more.
         </div>
       </div>
     </div>
