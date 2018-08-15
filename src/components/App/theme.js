4677f4e9a233fcec3dd900ba360de2b2ac3bd83f
Kevin Cooper <cooperka92@gmail.com>
2018-08-14 23:19:32 -0400

    :art: Add htmlFontSize to MUI theme

--- src/components/App/theme.js
+++ src/components/App/theme.js
@@ -1,2 +1,9 @@
 export const theme = {
+
+  // https://material-ui-next.com/customization/themes/#typography
+  typography: {
+    // Account for base font-size of 62.5%.
+    htmlFontSize: 10,
+  },
+
 };
