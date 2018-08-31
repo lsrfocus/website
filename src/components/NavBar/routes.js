dd944a90cda49157c565ab02cab081f48ec8a362
Kevin Cooper <cooperka92@gmail.com>
2018-08-30 22:24:13 -0400

    :bug: No need to import route components from DevPanel
    Fixes the [FLUSH CHUNKS] warnings.

--- src/components/NavBar/routes.js
+++ src/components/NavBar/routes.js
@@ -1,27 +1,18 @@
-import MainBody from '../MainBody/component';
-import Services from '../Services/component';
-import About from '../About/component';
-import Testimonials from '../Testimonials/component';
-
 const routes = [
   {
     path: '/',
-    component: MainBody,
     displayName: 'Home',
   },
   {
     path: '/services',
-    component: Services,
     displayName: 'Services',
   },
   {
     path: '/about',
-    component: About,
     displayName: 'About',
   },
   {
     path: '/testimonials',
-    component: Testimonials,
     displayName: 'Testimonials',
   },
 ];
