c6dafb70f162514d2da03535923ce11f3d71651a
Kevin Cooper <cooperka92@gmail.com>
2018-10-20 15:31:31 -0400

    :art: Shrink nav buttons on narrow screens

--- src/components/NavButton/component.js
+++ src/components/NavButton/component.js
@@ -1,12 +1,16 @@
 // @flow
 
+import * as R from 'ramda';
+import classNames from 'classnames';
 import React from 'react';
 import { NavLink } from 'react-static';
-import { Button } from 'material-ui';
+import { Button, withStyles } from 'material-ui';
+import { styles as buttonStyles } from 'material-ui/Button/Button';
 
 import './styles.css';
 
 type Props = {
+  classes: any,
   linkTo?: string,
   externalLinkTo?: string,
   bordered?: boolean,
@@ -14,13 +18,13 @@ type Props = {
 };
 
 function NavButton(props: Props) {
-  const { linkTo, externalLinkTo, bordered, children } = props;
+  const { classes, linkTo, externalLinkTo, bordered, children } = props;
   const isRouterLink = !!linkTo;
 
   return (
     <Button
       component={isRouterLink ? NavLink : 'a'}
-      className={['NavButton', bordered ? 'bordered' : ''].join(' ')}
+      className={classNames([classes.navButton, 'NavButton', bordered ? 'bordered' : ''])}
       to={linkTo}
       exact={isRouterLink ? true : undefined}
       href={externalLinkTo}
@@ -32,4 +36,12 @@ function NavButton(props: Props) {
   );
 }
 
-export default NavButton;
+const styles = (theme) => ({
+  navButton: {
+    [theme.breakpoints.down('xs')]: buttonStyles(theme).sizeSmall,
+  },
+});
+
+export default R.compose(
+  withStyles(styles),
+)(NavButton);
