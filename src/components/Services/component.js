6e9f3dfd970b096e67deb01833e878ed599e8555
Kevin Cooper <cooperka92@gmail.com>
2018-11-06 15:01:33 -0500

    :art: Update Services bullet points

--- src/components/Services/component.js
+++ src/components/Services/component.js
@@ -21,16 +21,17 @@ class Services extends Component<Props> {
           <div className="grid-container">
             <div className="grid-item">Web apps</div>
             <div className="grid-item">Mobile apps</div>
+            <div className="grid-item">Prototypes</div>
             <div className="grid-item">User testing</div>
-            <div className="grid-item">Project planning</div>
           </div>
         </div>
 
         <div className="section">
-          <div className="subtitle">Small business development</div>
+          <div className="subtitle">Business development</div>
           <div className="grid-container">
-            <div className="grid-item">Strategic planning</div>
-            <div className="grid-item">Team building</div>
+            <div className="grid-item">Strategic direction (&ldquo;what&rdquo;)</div>
+            <div className="grid-item">Strategic execution (&ldquo;how&rdquo;)</div>
+            <div className="grid-item">Team culture</div>
           </div>
         </div>
       </div>
