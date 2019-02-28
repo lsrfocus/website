da6a7c912c56a5c7398813a95213bbd57653b8d0
Kevin Cooper <cooperka92@gmail.com>
2019-02-27 23:27:11 -0500

    :art: Add some content to about-kevin

--- src/components/AboutMe/component.js
+++ src/components/AboutMe/component.js
@@ -15,6 +15,19 @@ class AboutMe extends Component<Props> {
     return (
       <div className="AboutMe page">
         <div className="title">About Kevin</div>
+
+        <div className="section">
+          <div className="description">
+            {'Kevin is the founder and owner of LSR Software. He lives in Ann Arbor with his partner Nina and two cats Midna and Ziri.'}
+          </div>
+          <div className="description">
+            {'Find out more at his '}
+            <a href="https://cooperka.com/">
+              personal website
+            </a>
+            {'.'}
+          </div>
+        </div>
       </div>
     );
   }
