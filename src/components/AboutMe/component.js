87912356b688297d8b1da46e0ce2c1b53846b380
Kevin Cooper <cooperka92@gmail.com>
2019-02-27 23:47:50 -0500

    :sparkles: Add ziri avatar for about-kevin

--- src/components/AboutMe/component.js
+++ src/components/AboutMe/component.js
@@ -4,21 +4,28 @@ import * as R from 'ramda';
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui';
 
+import avatar from '../../images/kevin-ziri-min.jpg';
+
 import './styles.css';
 
 type Props = {
+  classes: Object,
 };
 
 class AboutMe extends Component<Props> {
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="AboutMe page">
         <div className="title">About Kevin</div>
 
+        <img className={classes.avatar} src={avatar} alt="Kevin at his laptop with Ziri" />
+
         <div className="section">
           <div className="description">
-            {'Kevin is the founder and owner of LSR Software. He lives in Ann Arbor with his partner Nina and two cats Midna and Ziri.'}
+            {'Kevin is the founder and owner of LSR Software. He lives in Ann Arbor with his partner Nina and two cats Midna and Ziri (pictured).'}
           </div>
           <div className="description">
             {'Find out more at his '}
@@ -35,6 +42,12 @@ class AboutMe extends Component<Props> {
 }
 
 const styles = () => ({
+  avatar: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: 4,
+    width: 400,
+  },
 });
 
 export default R.compose(
